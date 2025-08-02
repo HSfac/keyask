699af40216570ff97fbdd434bc2438e32f21064e
HSfac <huiseong5278@gmail.com>
2025-08-02 14:53:52 +0900

    홈 페이지 배경 패턴 수정: 인라인 스타일로 변경하여 코드 가독성 향상 및 일관성 유지

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -222,7 +222,12 @@ export default function Home() {
       >
         {/* 배경 패턴 */}
         <div className="absolute inset-0 opacity-5">
-          <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width="100" height="100" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="%236366F1" fill-opacity="0.1"%3E%3Cpath d="M50 0a50 50 0 1 1 0 100 50 50 0 0 1 0-100z"/%3E%3C/g%3E%3C/svg%3E')]"></div>
+          <div 
+            className="absolute inset-0"
+            style={{
+              backgroundImage: `url("data:image/svg+xml,%3Csvg width='100' height='100' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%236366F1' fill-opacity='0.1'%3E%3Cpath d='M50 0a50 50 0 1 1 0 100 50 50 0 0 1 0-100z'/%3E%3C/g%3E%3C/svg%3E")`
+            }}
+          ></div>
         </div>
         
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative z-10">
